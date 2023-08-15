9b0b2a53d780770047e3dd1990f14e4c3edd4634
Sal-72 <salvadorsanchez059@gmail.com>
2023-08-15 15:23:44 +0000

    backup

---
+++ env_vars/set_vars.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env/python3
+
+import os
+
+def main():
+    os.environ["MY_VAR2"] = "Hello again, World!"
+    var1 = os.environ.get("MY_VAR2")
+    var2 = os.getenv("MY_VAR2")
+    print(var1, var2)
+
+    var3 = os.getenv("ABSENT_ENV", "I'm defined!")
+    print(var3)
+
+if __name__=="__main__":
+    main()
---
+++ unittest/greeting.py
@@ -0,0 +1,17 @@
+import random
+
+def test_create_greeting(self):
+        result = create_greeting("Hello", "Someone")
+        self.assertEqual(result, "Hello, Someone!")
+
+def create_greeting(greeting, name):
+    return greeting + "," + name + "!"
+
+def main():
+    greeting = random.choice(["Hello", "Howdy", "Yo", "What up"])
+    name = input("What is your name? ")
+    print(create_greeting(greeting, name))
+
+if __name__ == "__main__":
+    main()
+
